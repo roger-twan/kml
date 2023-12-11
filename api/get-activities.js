1a44585088b50aaefc6f8d9a24c0bacc52af853a
Roger Twan <roger.twan@gmail.com>
2023-12-11 18:21:55 +0800

    finish query API

---
+++ api/get-activities.js
@@ -0,0 +1,50 @@
+import { formatDateTime } from '../utils'
+
+export default async (request, env) => {
+  try {
+    let result = {
+      activities: {},
+      routes: [],
+      locations: [],
+    }
+
+    // activities
+    const activitiesData = await env.DB.prepare('SELECT * FROM activities').all()
+    for (const item of activitiesData.results) {
+      result.activities[item.id] = {
+        title: item.title,
+        activity_des: item.activity_des,
+        activity_date: item.activity_date,
+        link: item.link,
+      }
+    }
+    
+    // routes
+    const routesData = await env.DB.prepare('SELECT * FROM routes').all()
+    for (const item of routesData.results) {
+      result.routes.push({
+        activity_id: item.activity_id,
+        trip_mode: item.trip_mode,
+        duration: item.duration,
+        distance: item.distance,
+        coordinates: item.coordinates,
+      })
+    }
+
+    // locations
+    const locationsData = await env.DB.prepare('SELECT * FROM locations').all()
+    for (const item of locationsData.results) {
+      result.locations.push({
+        activity_id: item.activity_id,
+        location_name: item.location_name,
+        tag: item.tag,
+        coordinates: item.coordinates,
+      })
+    }
+  
+    return Response.json(result, { status: 200 })
+  } catch(e) {
+    console.error(e.toString())
+    return new Response(e.toString(), { status: 500 })
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { Router } from 'itty-router'
 import parseKml from './parse-kml'
 import saveActivity from './api/save-activity'
+import getActivities from './api/get-activities'
 
 const router = Router()
 
 router.get('/parse', parseKml)
-router.post('/api/save_activity', saveActivity )
+router.post('/api/save_activity', saveActivity)
+router.get('/api/get_activities', getActivities)
 router.all('*', () => new Response('404', { status: 404 }));
 
 export default {
